2cc94cf7b251494726ceb5530a25c36a9337416c
redoc <619675912@qq.com>
2021-05-28 14:24:48 +0800

    Create drv_rtc.h

---
+++ drv_rtc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019-2020, redoc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-01-04     redoc        the first version
+ */
+
+#ifndef __DRV_RTC_H
+#define __DRV_RTC_H
+
+#include <rtthread.h>
+#include "rtdevice.h"
+#include <drv_common.h>
+
+#endif
